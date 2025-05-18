9f692a3fe1775eecead4711b3f86fa78989931c8
POLLEN8 Staff <163085852+pollen8llc@users.noreply.github.com>
2025-05-18 17:28:27 -0400

    Delete src/components/community directory

--- src/components/community/form/index.ts
+++
@@ -1,6 +0,0 @@
-
-export { default as UnifiedCommunityForm } from './UnifiedCommunityForm';
-export { default as BasicDetailsStep } from './steps/BasicDetailsStep';
-export { default as LocationFormatStep } from './steps/LocationFormatStep';
-export { default as OnlinePresenceStep } from './steps/OnlinePresenceStep';
-export { default as AdditionalDetailsStep } from './steps/AdditionalDetailsStep';
