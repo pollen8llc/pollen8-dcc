6cf21b390e2cca5eff4886278b8ad3bcacdf0da5
POLLEN8 Staff <163085852+pollen8llc@users.noreply.github.com>
2025-05-18 17:38:08 -0400

    Delete src/repositories/community directory

--- src/repositories/community/communityQueryRepository.ts
+++
@@ -1,6 +0,0 @@
-
-// Export all query functions from a single entry point
-export * from "./queries/getAllCommunities";
-export * from "./queries/getCommunityById";
-export * from "./queries/searchCommunities";
-export * from "./queries/getManagedCommunities";
--- src/repositories/community/index.ts
+++
@@ -1,5 +0,0 @@
-
-// Export all community repository functions from a single entry point
-export * from "./communityQueryRepository";
-export * from "./communityMutationRepository";
-export * from "./communityMembershipRepository";
