e5cb472a064bab5da439faf14ef29a26d77319e9
POLLEN8 Staff <163085852+pollen8llc@users.noreply.github.com>
2025-05-31 16:06:10 -0400

    Update ContactList.tsx

--- src/components/rel8t/ContactList.tsx
+++ src/components/rel8t/ContactList.tsx
@@ -42,7 +42,6 @@ const ContactList: React.FC<ContactListProps> = ({
   }
 
   return (
-    <div className="max-h-[500px] overflow-y-auto pr-2">
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 p-1">
         {contacts.map((contact) => (
           <ContactCard 
@@ -55,7 +54,6 @@ const ContactList: React.FC<ContactListProps> = ({
           />
         ))}
       </div>
-    </div>
   );
 };
 
